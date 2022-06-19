ef44c0c0ba2facacd2431f557cc6c942a7977217
Your Name <you@example.com>
2022-06-19 15:20:53 +0900

    feat: add sounds

--- js/data.js
+++ js/data.js
@@ -1,3 +1,8 @@
-{
-    
-}
+
+let wordData = [
+    ["hiragana", "あ", "い", "う", "え", "お", "あ.mp3", "い.mp3", "う.mp3", "え.mp3", "お.mp3"],
+    ["Lao-JP", "LaoWord", "JPWord", "JPKanzi", "", "", ""]
+];
+//    1:{"LAOWord":"laoWord", "JPWord":"JPWord", "JPKanzi":"JPKanzi"}
+
+
--- js/page_body.js
+++ js/page_body.js
@@ -2,7 +2,72 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+const setting = {
+  sizeAtPage: 10
+};
+
+let voice = new Audio();
+const voiceDir = "sounds/"
+console.log("start");
+
+function pronounce(voiceStr){
+  let voicePath = voiceDir+voiceStr;
+  voice.pause();
+  voice.setAttribute('src', voicePath); 
+  voice.load(); 
+  voice.play(); 
+  //alert(voiceStr);
+  console.log(voicePath);
+}
+
+function createWordTable(data, page){
+  
+  let tr_list = [];
+
+  for(let i = 0; i < setting.sizeAtPage; i++){
+    let dataIndex = setting.sizeAtPage * page + i;
+
+    if (dataIndex >= data.length){
+      break;
+    }
+
+    let tr_elem = data[dataIndex];
+    let tdList = [];
+
+    if (tr_elem[0] == "hiragana" || tr_elem[0] == "Lao-JP"){
+      for(let j = 1; j < tr_elem.length/2 ; j++){
+        let voiceStr = tr_elem[parseInt(j+(tr_elem.length-1)/2)];
+        tdList.push(
+          e("td", 
+            {key: 'column'+String(j)+'_row'+String(i), className:"hoge", 
+              onClick: () =>{pronounce(voiceStr);}},
+            tr_elem[j])
+        );
+      }
+    }
+
+   tr_list.push(e("tr", {key: 'row'+String(i)},tdList));
+  }
+  
+  return (
+    e("div", { id: 'topView'},
+      e('button',{id:'button', onClick: () =>{alert('test');}},"test"), 
+      e("table", { className: 'viewTable'},
+        e("tbody", {},tr_list)
+      )
+    )
+  );
+  
+  /* ok 
+ return e(
+  'button',
+  { onClick: () =>{alert('test'); }},
+  'Like'
+  );
+  */
+}
+
+class WordTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = { page: 0};
@@ -18,11 +83,12 @@ class LikeButton extends React.Component {
     return e(
       'button',
       { onClick: () =>{ 
-        this.setState({ liked: true }) 
+        alert('test'); 
       }},
       'Like'
     );
     */
+    
     /*
     return (
       e("ul", { className: 'shiritori' }, 
@@ -33,27 +99,12 @@ class LikeButton extends React.Component {
       )
     );
     */
-   return (
-    e("div", { className: 'shiritori' },
-      e('button',{},"test"), 
-      e("table", { className: 'shiritori' },
-        e("tbody", {},
-          e("tr", {},
-            e("td", {}, "aa"),
-            e("td", {}, "ai")
-          ),
-          e("tr", {},
-            e("td", {}, "aa"),
-            e("td", {}, "ai")
-          )
-        )
-      )
-    )
-  );
-
+    
+    
+    return createWordTable(wordData, this.state.page);
   }
 }
 
 const domContainer = document.querySelector('#page_body');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(e(WordTable));
